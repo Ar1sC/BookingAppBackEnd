9f1f25d1e4d45e748330160e10767777e329288b
Aris <Ar1sC@users.noreply.github.com>
2024-05-08 15:38:51 +0300

    BookingAPP Using MongoDB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoute from "./routes/auth.js"
+import usersRoute from "./routes/users.js"
+import hotelsRoute from "./routes/hotels.js"
+import roomsRoute from "./routes/rooms.js"
 const app = express();
 dotenv.config();
 
@@ -24,6 +28,12 @@ mongoose.connection.on("connected", ()=>{
 app.get("/", (req,res)=> {
     res.send("Hello First Request!")
 })
+
+//middlewares
+app.use("/api/auth", authRoute)
+app.use("/api/users", authRoute)
+app.use("/api/hotels", authRoute)
+app.use("/api/rooms", authRoute)
 
 app.listen(8800, ()=> {
     connect()
---
+++ api/routes/hotels.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const router = express.Router();
+
+//CREATE
+router.post("/", async (req,res)=>{
+    try {
+
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+//UPDATE
+//DELETE
+//GET
+//GET ALL
+
+
+export default router
